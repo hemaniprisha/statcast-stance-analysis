2771525fef556e496c32da3b6eae1aa0a6619fa8
Prisha Hemani <hemaniprisha1@gmail.com>
2025-08-27 19:25:16 -0700

    Modified comments on create_sample.py

--- src/create_sample.py
+++ src/create_sample.py
@@ -1,8 +1,8 @@
+# Creates sample dataset from the combined_statcast_data.csv file
 import pandas as pd
 
 df = pd.read_csv("../data/combined_statcast_data.csv")
 
-# Old-style, works on all versions, still triggers a FutureWarning
 sample_df = df.groupby("player_name", group_keys=False).apply(
     lambda x: x.sample(min(len(x), 100))
 )
